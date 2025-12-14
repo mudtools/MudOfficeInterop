16717775c2713cc26cf6e7088ee490da3e731923
倔强的石头 <413901125@qq.com>
2025-12-14 21:31:25 +0800

    feat(excel): 更新 Excel 最近文件接口定义并调整实现

--- MudTools.OfficeInterop.Excel/Content/Misc/IExcelRecentFile.cs
+++ MudTools.OfficeInterop.Excel/Content/Misc/IExcelRecentFile.cs
@@ -1,5 +1,5 @@
 ﻿//
-// 懒人Excel工具箱 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// MudTools.OfficeInterop 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -11,6 +11,7 @@ namespace MudTools.OfficeInterop.Excel;
 /// Excel RecentFile 对象的二次封装接口
 /// 提供对 Microsoft.Office.Interop.Excel.RecentFile 的安全访问和操作
 /// </summary>
+[ComObjectWrap(ComNamespace = "MsExcel")]
 public interface IExcelRecentFile : IDisposable
 {
     #region 基础属性
@@ -24,6 +25,7 @@ public interface IExcelRecentFile : IDisposable
     /// 获取最近使用文件所在的Application对象
     /// 对应 RecentFile.Application 属性
     /// </summary>
+    [ComPropertyWrap(NeedDispose = false)]
     IExcelApplication Application { get; }
     /// <summary>
     /// 获取最近使用文件的名称 (通常包含路径)
@@ -44,22 +46,13 @@ public interface IExcelRecentFile : IDisposable
     string Path { get; }
     #endregion
 
-    #region 状态属性
-    /// <summary>
-    /// 获取最近使用文件是否仍然存在
-    /// </summary>
-    bool Exists { get; }
-    #endregion
-
     #region 操作方法
     /// <summary>
     /// 打开此最近使用的文件
     /// 对应 RecentFile.Open 方法
     /// </summary>
-    /// <param name="readOnly">是否以只读方式打开</param>
-    /// <param name="notify">打开时是否通知</param>
     /// <returns>打开的工作簿对象</returns>
-    IExcelWorkbook Open(bool readOnly = false, bool notify = false); // 假设 IExcelWorkbook 存在
+    IExcelWorkbook Open();
 
     /// <summary>
     /// 删除此最近使用文件记录 (从 RecentFiles 集合中移除)
