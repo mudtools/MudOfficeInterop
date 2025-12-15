df603db57e064e274bc09153df96c4c105105083
倔强的石头 <413901125@qq.com>
2025-12-15 16:40:41 +0800

    feat(excel): 更新Excel连接接口并移除旧实现
    - 在IExcelConnections接口上添加ComCollectionWrap特性
    - 添加Parent属性和Application属性
    - 移除GetItem方法
    - 移除FindByName和FindByType方法
    - 移除Remove和Clear方法
    - 移除Parent属性（重复定义）
    - 移除RefreshAll方法
    - 添加Add2和AddFromFile2方法支持更多参数
    - 删除ExcelConnections实现类文件

--- MudTools.OfficeInterop.Excel/Content/Data/IExcelConnections.cs
+++ MudTools.OfficeInterop.Excel/Content/Data/IExcelConnections.cs
@@ -1,5 +1,5 @@
 //
-// 懒人Excel工具箱 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// MudTools.OfficeInterop 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -9,10 +9,23 @@ namespace MudTools.OfficeInterop.Excel;
 
 /// <summary>
 /// 表示Excel工作簿中的连接集合接口
-/// 继承自IDisposable和IEnumerable<IExcelWorkbookConnection>接口，提供对Excel工作簿连接的管理功能
+/// 继承自IDisposable和IEnumerable[IExcelWorkbookConnection]接口，提供对Excel工作簿连接的管理功能
 /// </summary>
+[ComCollectionWrap(ComNamespace = "MsExcel")]
 public interface IExcelConnections : IDisposable, IEnumerable<IExcelWorkbookConnection>
 {
+
+    /// <summary>
+    /// 获取父级工作簿
+    /// </summary>
+    object Parent { get; }
+
+    /// <summary>
+    /// 获取当前对象所在的Application对象
+    /// </summary>
+    [ComPropertyWrap(NeedDispose = false)]
+    IExcelApplication Application { get; }
+
     /// <summary>
     /// 获取连接集合中的连接数量
     /// </summary>
@@ -32,12 +45,6 @@ public interface IExcelConnections : IDisposable, IEnumerable<IExcelWorkbookConn
     /// <returns>工作簿连接对象</returns>
     IExcelWorkbookConnection this[string name] { get; }
 
-    /// <summary>
-    /// 根据索引或名称获取连接
-    /// </summary>
-    /// <param name="index">连接索引或名称</param>
-    /// <returns>工作簿连接对象</returns>
-    IExcelWorkbookConnection GetItem(object index);
 
     /// <summary>
     /// 添加新的工作簿连接
@@ -52,37 +59,35 @@ public interface IExcelConnections : IDisposable, IEnumerable<IExcelWorkbookConn
                                 string? commandText = null, XlCmdType lCmdType = XlCmdType.xlCmdSql);
 
     /// <summary>
-    /// 根据名称查找连接
+    /// 添加新的工作簿连接（增强版）
     /// </summary>
     /// <param name="name">连接名称</param>
-    /// <returns>工作簿连接对象</returns>
-    IExcelWorkbookConnection FindByName(string name);
+    /// <param name="description">连接描述</param>
+    /// <param name="connectionString">连接字符串</param>
+    /// <param name="commandText">命令文本</param>
+    /// <param name="lCmdtype">命令类型</param>
+    /// <param name="createModelConnection">是否创建模型连接</param>
+    /// <param name="importRelationships">是否导入关系</param>
+    /// <returns>新创建的工作簿连接对象</returns>
+    IExcelWorkbookConnection Add2(string name, string description,
+        string connectionString, string? commandText = null,
+        XlCmdType lCmdtype = XlCmdType.xlCmdSql, bool? createModelConnection = null,
+        bool? importRelationships = null);
+
 
     /// <summary>
-    /// 根据连接类型查找连接
+    /// 从文件添加新的工作簿连接
     /// </summary>
-    /// <param name="connectionType">连接类型</param>
-    /// <returns>连接对象枚举</returns>
-    IEnumerable<IExcelWorkbookConnection> FindByType(XlConnectionType connectionType);
+    /// <param name="filename">文件名</param>
+    /// <returns>新创建的工作簿连接对象</returns>
+    IExcelWorkbookConnection AddFromFile(string filename);
 
     /// <summary>
-    /// 移除指定名称的连接
+    /// 从文件添加新的工作簿连接（增强版）
     /// </summary>
-    /// <param name="name">连接名称</param>
-    void Remove(string name);
-
-    /// <summary>
-    /// 清除所有连接
-    /// </summary>
-    void Clear();
-
-    /// <summary>
-    /// 获取父级工作簿
-    /// </summary>
-    IExcelWorkbook Parent { get; }
-
-    /// <summary>
-    /// 刷新所有连接
-    /// </summary>
-    void RefreshAll();
+    /// <param name="filename">文件名</param>
+    /// <param name="createModelConnection">是否创建模型连接</param>
+    /// <param name="importRelationships">是否导入关系</param>
+    /// <returns>新创建的工作簿连接对象</returns>
+    IExcelWorkbookConnection AddFromFile2(string filename, bool? createModelConnection, bool? importRelationships);
 }
