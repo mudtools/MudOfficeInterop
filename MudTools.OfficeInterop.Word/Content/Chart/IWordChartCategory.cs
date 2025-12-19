a530e44b2ccb52efa4ac26b0a521d69d750e4041
倔强的石头 <413901125@qq.com>
2025-12-19 22:17:21 +0800

    feat(OfficeInterop.Word): 更新图表分类接口并移除旧实现类
    为 IWordChartCategory 接口添加 ComObjectWrap 特性以支持 COM 对象包装，
    并将 Parent 属性设置为可空类型以提高安全性。
    同时删除了已弃用的 WordChartCategory 实现类。

--- MudTools.OfficeInterop.Word/Content/Chart/IWordChartCategory.cs
+++ MudTools.OfficeInterop.Word/Content/Chart/IWordChartCategory.cs
@@ -4,12 +4,13 @@ namespace MudTools.OfficeInterop.Word;
 /// <summary>
 /// 表示 Word 图表分类的封装接口。
 /// </summary>
+[ComObjectWrap(ComNamespace = "MsWord")]
 public interface IWordChartCategory : IDisposable
 {
     /// <summary>
     /// 获取父对象。
     /// </summary>
-    object Parent { get; }
+    object? Parent { get; }
 
     /// <summary>
     /// 获取分类名称。
