88a450143c9cb3aa958538b0f08d1ad507e3a199
倔强的石头 <413901125@qq.com>
2025-12-21 10:42:54 +0800

    feat(IWordWalls): 更新接口定义以支持更多图表墙壁功能
    新增 `Paste` 和 `ClearFormats` 方法，并更新属性类型以提高兼容性和功能性。
    增强对 COM 对象的封装控制，包括添加 `ComPropertyWrap` 特性以及可空引用类型支持。

--- MudTools.OfficeInterop.Word/Content/Chart/IWordWalls.cs
+++ MudTools.OfficeInterop.Word/Content/Chart/IWordWalls.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // MudTools.OfficeInterop 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
@@ -10,17 +10,19 @@ namespace MudTools.OfficeInterop.Word;
 /// <summary>
 /// 表示 Word 图表墙壁的封装接口。
 /// </summary>
+[ComObjectWrap(ComNamespace = "MsWord")]
 public interface IWordWalls : IDisposable
 {
     /// <summary>
     /// 获取应用程序对象。
     /// </summary>
-    IWordApplication Application { get; }
+    [ComPropertyWrap(NeedDispose = false, NeedConvert = true)]
+    IWordApplication? Application { get; }
 
     /// <summary>
     /// 获取父对象。
     /// </summary>
-    object Parent { get; }
+    object? Parent { get; }
 
     /// <summary>
     /// 获取或设置墙壁名称。
@@ -31,6 +33,12 @@ public interface IWordWalls : IDisposable
     /// 获取或设置厚度。
     /// </summary>
     int Thickness { get; set; }
+
+    /// <summary>
+    /// 获取或设置图片单位大小。
+    /// </summary>
+    [ComPropertyWrap(NeedConvert = true)]
+    int PictureUnit { get; set; }
 
     /// <summary>
     /// 获取内部区域格式。
@@ -55,10 +63,21 @@ public interface IWordWalls : IDisposable
     /// <summary>
     /// 获取或设置是否显示图片背面。
     /// </summary>
-    bool PictureType { get; set; }
+    XlChartPictureType PictureType { get; set; }
 
     /// <summary>
     /// 选择墙壁。
     /// </summary>
     void Select();
+
+    /// <summary>
+    /// 将剪贴板的内容粘贴到墙壁上。
+    /// </summary>
+    void Paste();
+
+    /// <summary>
+    /// 清除指定对象的格式设置并将格式设置恢复为默认值。
+    /// </summary>
+    /// <returns>返回清除格式后的对象。</returns>
+    object? ClearFormats();
 }
