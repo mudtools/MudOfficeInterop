66d79be6d0c013377fcb2be11e1dbd32905cdefa
倔强的石头 <413901125@qq.com>
2025-12-23 22:25:53 +0800

    refactor(IWordWords): 更新接口定义并移除实现类
    - 为 IWordWords 接口添加 [ComCollectionWrap] 特性标注
    - 将 IEnumerable<IWordRange> 更改为 IEnumerable<IWordRange?> 以支持可空类型
    - 为 Application 属性添加 [ComPropertyWrap] 特性并标记为 NeedDispose = false
    - 将 Application、Parent、索引器、First 和 Last 属性的返回类型改为可空类型
    - 移除 WordWords.cs 实现类中的所有方法实现，只保留基本接口定义
    - 删除了大量具体的实现方法，包括 GetRange、Find、Replace、Add、Insert、Delete 等

--- MudTools.OfficeInterop.Word/Content/Text/IWordWords.cs
+++ MudTools.OfficeInterop.Word/Content/Text/IWordWords.cs
@@ -10,17 +10,19 @@ namespace MudTools.OfficeInterop.Word;
 /// <summary>
 /// 表示 Word 单词集合的封装接口。
 /// </summary>
-public interface IWordWords : IEnumerable<IWordRange>, IDisposable
+[ComCollectionWrap(ComNamespace = "MsWord")]
+public interface IWordWords : IEnumerable<IWordRange?>, IDisposable
 {
     /// <summary>
     /// 获取应用程序对象。
     /// </summary>
-    IWordApplication Application { get; }
+    [ComPropertyWrap(NeedDispose = false)]
+    IWordApplication? Application { get; }
 
     /// <summary>
     /// 获取父对象。
     /// </summary>
-    object Parent { get; }
+    object? Parent { get; }
 
     /// <summary>
     /// 获取单词数量。
@@ -30,131 +32,15 @@ public interface IWordWords : IEnumerable<IWordRange>, IDisposable
     /// <summary>
     /// 通过索引获取单词范围。
     /// </summary>
-    IWordRange this[int index] { get; }
+    IWordRange? this[int index] { get; }
 
     /// <summary>
     /// 获取第一个单词。
     /// </summary>
-    IWordRange First { get; }
+    IWordRange? First { get; }
 
     /// <summary>
     /// 获取最后一个单词。
     /// </summary>
-    IWordRange Last { get; }
-
-    /// <summary>
-    /// 获取指定范围的单词。
-    /// </summary>
-    /// <param name="start">开始位置。</param>
-    /// <param name="end">结束位置。</param>
-    /// <returns>单词范围对象。</returns>
-    IWordRange GetRange(int start, int end);
-
-    /// <summary>
-    /// 查找指定文本。
-    /// </summary>
-    /// <param name="findText">要查找的文本。</param>
-    /// <param name="forward">是否向前查找。</param>
-    /// <param name="wrap">是否循环查找。</param>
-    /// <param name="matchCase">是否匹配大小写。</param>
-    /// <param name="matchWholeWord">是否匹配整个单词。</param>
-    /// <param name="matchWildcards">是否使用通配符。</param>
-    /// <param name="matchSoundsLike">是否匹配发音。</param>
-    /// <param name="matchAllWordForms">是否匹配所有词形。</param>
-    /// <returns>找到的单词范围，未找到返回null。</returns>
-    IWordRange Find(string findText, bool forward = true, WdFindWrap wrap = WdFindWrap.wdFindStop,
-                   bool matchCase = false, bool matchWholeWord = false, bool matchWildcards = false,
-                   bool matchSoundsLike = false, bool matchAllWordForms = false);
-
-    /// <summary>
-    /// 替换指定文本。
-    /// </summary>
-    /// <param name="findText">要查找的文本。</param>
-    /// <param name="replaceText">替换文本。</param>
-    /// <param name="replaceAll">是否替换所有匹配项。</param>
-    /// <param name="matchCase">是否匹配大小写。</param>
-    /// <param name="matchWholeWord">是否匹配整个单词。</param>
-    /// <param name="matchWildcards">是否使用通配符。</param>
-    /// <param name="matchSoundsLike">是否匹配发音。</param>
-    /// <param name="matchAllWordForms">是否匹配所有词形。</param>
-    /// <returns>替换的次数。</returns>
-    int Replace(string findText, string replaceText, bool replaceAll = false,
-                bool matchCase = false, bool matchWholeWord = false, bool matchWildcards = false,
-                bool matchSoundsLike = false, bool matchAllWordForms = false);
-
-    /// <summary>
-    /// 添加文本到末尾。
-    /// </summary>
-    /// <param name="text">要添加的文本。</param>
-    /// <returns>新添加的单词范围。</returns>
-    IWordRange Add(string text);
-
-    /// <summary>
-    /// 插入文本到指定位置。
-    /// </summary>
-    /// <param name="index">插入位置。</param>
-    /// <param name="text">要插入的文本。</param>
-    /// <returns>新插入的单词范围。</returns>
-    IWordRange Insert(int index, string text);
-
-    /// <summary>
-    /// 删除指定范围的单词。
-    /// </summary>
-    /// <param name="start">开始位置。</param>
-    /// <param name="count">删除单词数。</param>
-    void Delete(int start, int count);
-
-    /// <summary>
-    /// 清除所有单词内容。
-    /// </summary>
-    void Clear();
-
-    /// <summary>
-    /// 获取所有单词索引列表。
-    /// </summary>
-    /// <returns>单词索引列表。</returns>
-    List<int> GetIndexes();
-
-    /// <summary>
-    /// 获取文本内容。
-    /// </summary>
-    /// <returns>完整文本内容。</returns>
-    string GetText();
-
-    /// <summary>
-    /// 设置文本内容。
-    /// </summary>
-    /// <param name="text">新的文本内容。</param>
-    void SetText(string text);
-
-    /// <summary>
-    /// 获取指定范围的子单词集合。
-    /// </summary>
-    /// <param name="startIndex">开始索引。</param>
-    /// <param name="length">长度。</param>
-    /// <returns>子单词集合范围。</returns>
-    IWordRange Substring(int startIndex, int length);
-
-    /// <summary>
-    /// 检查是否包含指定文本。
-    /// </summary>
-    /// <param name="text">要检查的文本。</param>
-    /// <param name="matchCase">是否匹配大小写。</param>
-    /// <returns>是否包含。</returns>
-    bool Contains(string text, bool matchCase = false);
-
-    /// <summary>
-    /// 获取指定单词的字符数。
-    /// </summary>
-    /// <param name="index">单词索引。</param>
-    /// <returns>字符数量。</returns>
-    int GetCharacterCount(int index);
-
-    /// <summary>
-    /// 统计指定范围内的句子数。
-    /// </summary>
-    /// <param name="startIndex">开始索引。</param>
-    /// <param name="length">长度。</param>
-    /// <returns>句子数量。</returns>
-    int GetSentenceCount(int startIndex, int length);
+    IWordRange? Last { get; }
 }
