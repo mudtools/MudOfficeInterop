7418f8f59991d26771453ae479e83e3f5159286e
倔强的石头 <413901125@qq.com>
2025-12-29 19:25:07 +0800

    feat(word): 更新Word批注接口实现并删除旧的实现类
    - 为IWordComment接口添加ComObjectWrap特性并继承IOfficeObject
    - 为IWordComments接口添加ComCollectionWrap特性并更新泛型类型为IWordComment?
    - 更新IWordComment接口的属性定义，修改Range、Reference、Scope等属性的描述
    - 重定义IWordComments接口的方法，移除过多多余方法，只保留核心功能
    - 移除WordComment.cs和WordComments.cs两个具体的实现类
    - 为Application属性添加ComPropertyWrap特性
    - 更新接口中方法的参数和返回值类型，统一使用可空类型

--- MudTools.OfficeInterop.Word/Helpers/Assist/IWordComments.cs
+++ MudTools.OfficeInterop.Word/Helpers/Assist/IWordComments.cs
@@ -8,13 +8,15 @@
 namespace MudTools.OfficeInterop.Word;
 
 /// <summary>
-/// 表示 Word 批注集合的封装接口。
+/// 表示 Comment 对象的集合，这些对象表示选择、范围或文档中的批注。
 /// </summary>
-public interface IWordComments : IEnumerable<IWordComment>, IDisposable
+[ComCollectionWrap(ComNamespace = "MsWord")]
+public interface IWordComments : IEnumerable<IWordComment?>, IOfficeObject<IWordComments>, IDisposable
 {
     /// <summary>
     /// 获取应用程序对象。
     /// </summary>
+    [ComPropertyWrap(NeedDispose = false)]
     IWordApplication? Application { get; }
 
     /// <summary>
@@ -28,104 +30,22 @@ public interface IWordComments : IEnumerable<IWordComment>, IDisposable
     int Count { get; }
 
     /// <summary>
-    /// 通过索引获取批注。
+    /// 获取或设置其批注显示在批注窗格中的审阅者名称。
     /// </summary>
-    IWordComment this[int index] { get; }
+    string ShowBy { get; set; }
 
     /// <summary>
-    /// 获取第一个批注。
+    /// 返回集合中的单个对象。
     /// </summary>
-    IWordComment First { get; }
+    /// <param name="index">指示单个对象序数位置的整数。</param>
+    /// <returns>指定索引处的 Comment 对象。</returns>
+    IWordComment? this[int index] { get; }
 
     /// <summary>
-    /// 获取最后一个批注。
+    /// 返回表示添加到范围的批注的 Comment 对象。
     /// </summary>
-    IWordComment Last { get; }
-
-    /// <summary>
-    /// 添加新的批注。
-    /// </summary>
-    /// <param name="range">批注范围。</param>
-    /// <param name="text">批注文本。</param>
-    /// <param name="author">批注作者。</param>
-    /// <returns>新创建的批注。</returns>
-    IWordComment Add(IWordRange range, string text = null, string author = null);
-
-    /// <summary>
-    /// 删除指定索引的批注。
-    /// </summary>
-    /// <param name="index">批注索引。</param>
-    void Delete(int index);
-
-    /// <summary>
-    /// 删除指定范围的批注。
-    /// </summary>
-    /// <param name="startIndex">开始索引。</param>
-    /// <param name="count">删除数量。</param>
-    void DeleteRange(int startIndex, int count);
-
-    /// <summary>
-    /// 删除所有批注。
-    /// </summary>
-    void Clear();
-
-    /// <summary>
-    /// 获取所有批注索引列表。
-    /// </summary>
-    /// <returns>批注索引列表。</returns>
-    List<int> GetIndexes();
-
-    /// <summary>
-    /// 获取文档总批注字符数。
-    /// </summary>
-    /// <returns>字符总数。</returns>
-    int GetTotalCharacters();
-
-    /// <summary>
-    /// 获取文档总批注单词数。
-    /// </summary>
-    /// <returns>单词总数。</returns>
-    int GetTotalWords();
-
-    /// <summary>
-    /// 按日期范围筛选批注。
-    /// </summary>
-    /// <param name="startDate">开始日期。</param>
-    /// <param name="endDate">结束日期。</param>
-    /// <returns>批注列表。</returns>
-    List<IWordComment> GetByDateRange(DateTime startDate, DateTime endDate);
-
-    /// <summary>
-    /// 按作者筛选批注。
-    /// </summary>
-    /// <param name="author">作者名称。</param>
-    /// <returns>批注列表。</returns>
-    List<IWordComment> GetByAuthor(string author);
-
-    /// <summary>
-    /// 查找包含指定文本的批注。
-    /// </summary>
-    /// <param name="text">要查找的文本。</param>
-    /// <param name="matchCase">是否匹配大小写。</param>
-    /// <param name="matchWholeWord">是否匹配整个单词。</param>
-    /// <returns>批注列表。</returns>
-    List<IWordComment> FindByText(string text, bool matchCase = false, bool matchWholeWord = false);
-
-    /// <summary>
-    /// 替换所有批注中的指定文本。
-    /// </summary>
-    /// <param name="findText">要查找的文本。</param>
-    /// <param name="replaceText">替换文本。</param>
-    /// <param name="matchCase">是否匹配大小写。</param>
-    /// <param name="matchWholeWord">是否匹配整个单词。</param>
-    /// <returns>总替换次数。</returns>
-    int ReplaceAllText(string findText, string replaceText, bool matchCase = false, bool matchWholeWord = false);
-
-    /// <summary>
-    /// 获取指定范围的批注。
-    /// </summary>
-    /// <param name="startIndex">开始索引。</param>
-    /// <param name="endIndex">结束索引。</param>
-    /// <returns>批注列表。</returns>
-    List<IWordComment> GetRange(int startIndex, int endIndex);
+    /// <param name="range">必需。Range 对象。要添加批注的范围。</param>
+    /// <param name="text">可选项。批注的文本。</param>
+    /// <returns>新创建的 Comment 对象。</returns>
+    IWordComment? Add(IWordRange range, string? text = null);
 }
