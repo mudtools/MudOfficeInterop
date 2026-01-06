87d457ba8ed229fb41600d28e8a47dec9a5ef38d
倔强的石头 <413901125@qq.com>
2026-01-06 19:56:32 +0800

    feat(word): 更新IWordInlineShapes接口增强Word内嵌形状功能
    - 添加ComCollectionWrap特性标注接口
    - 修改接口继承IOfficeObject以支持更好的对象包装
    - 将返回类型从IWordInlineShape更新为IWordInlineShape?以支持空值
    - 添加New方法用于插入图片对象
    - 添加AddHorizontalLine重载方法支持标准水平线
    - 添加AddPictureBullet方法用于图片项目符号
    - 添加AddChart2方法支持图表样式
    - 添加AddSmartArt方法用于SmartArt图形
    - 添加AddWebVideo方法用于Web视频
    - 移除旧的WordInlineShapes实现类
    - 更新方法参数为可空类型以提高类型安全性
    - 改进方法文档注释提供更详细的参数说明

--- MudTools.OfficeInterop.Word/Formatting/Graph/IWordInlineShapes.cs
+++ MudTools.OfficeInterop.Word/Formatting/Graph/IWordInlineShapes.cs
@@ -10,11 +10,13 @@ namespace MudTools.OfficeInterop.Word;
 /// <summary>
 /// 表示 Word 中内嵌形状集合的封装接口
 /// </summary>
-public interface IWordInlineShapes : IEnumerable<IWordInlineShape>, IDisposable
+[ComCollectionWrap(ComNamespace = "MsWord")]
+public interface IWordInlineShapes : IEnumerable<IWordInlineShape?>, IOfficeObject<IWordInlineShapes, MsWord.InlineShapes>, IDisposable
 {
     /// <summary>
     /// 获取应用程序对象。
     /// </summary>
+    [ComPropertyWrap(NeedDispose = false)]
     IWordApplication? Application { get; }
 
     /// <summary>
@@ -32,7 +34,14 @@ public interface IWordInlineShapes : IEnumerable<IWordInlineShape>, IDisposable
     /// </summary>
     /// <param name="index">内嵌形状索引（从1开始）</param>
     /// <returns>内嵌形状对象</returns>
-    IWordInlineShape this[int index] { get; }
+    IWordInlineShape? this[int index] { get; }
+
+    /// <summary>
+    /// 插入一个由边框环绕的空白的 1 英寸见方的 Microsoft Word 图片对象。
+    /// </summary>
+    /// <param name="range">必需。Range 对象。新图形的位置。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? New(IWordRange range);
 
     /// <summary>
     /// 添加图片内嵌形状
@@ -41,7 +50,7 @@ public interface IWordInlineShapes : IEnumerable<IWordInlineShape>, IDisposable
     /// <param name="linkToFile">是否链接到文件</param>
     /// <param name="saveWithDocument">是否与文档一起保存</param>
     /// <returns>新添加的内嵌形状对象</returns>
-    IWordInlineShape AddPicture(string fileName, bool linkToFile = false, bool saveWithDocument = true);
+    IWordInlineShape? AddPicture(string fileName, bool linkToFile = false, bool saveWithDocument = true);
 
     /// <summary>
     /// 添加OLE对象内嵌形状
@@ -54,41 +63,70 @@ public interface IWordInlineShapes : IEnumerable<IWordInlineShape>, IDisposable
     /// <param name="iconIndex">图标索引</param>
     /// <param name="iconLabel">图标标签</param>
     /// <returns>新创建的内嵌形状对象</returns>
-    IWordInlineShape AddOLEObject(string classType = null, string fileName = null, bool linkToFile = false,
-                                 bool displayAsIcon = false, string iconFileName = null, int iconIndex = 0,
-                                 string iconLabel = null);
-
-    /// <summary>
-    /// 添加水平线内嵌形状
-    /// </summary>
-    /// <param name="fileName">水平线文件路径</param>
-    /// <param name="Range">是否链接到文件</param>
-    /// <returns>新创建的内嵌形状对象</returns>
-    IWordInlineShape AddHorizontalLine(string fileName = null, object Range = null);
+    IWordInlineShape? AddOLEObject(string? classType = null, string? fileName = null, bool? linkToFile = false,
+                                 bool? displayAsIcon = false, string? iconFileName = null, int? iconIndex = 0,
+                                 string? iconLabel = null);
 
 
     /// <summary>
-    /// 添加图表内嵌形状
+    /// 基于图像文件向当前文档添加水平线。
     /// </summary>
-    /// <param name="style">图表样式</param>
-    /// <returns>新创建的内嵌形状对象</returns>
-    IWordInlineShape AddChart(MsoChartType style = MsoChartType.xlArea);
+    /// <param name="fileName">必需。要用作水平线的图像的文件名。</param>
+    /// <param name="range">可选项。Microsoft Word 放置水平线之上的范围。如果省略此参数，Word 将在当前选定内容之上放置水平线。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? AddHorizontalLine(string fileName, IWordRange? range = null);
 
     /// <summary>
-    /// 根据索引删除内嵌形状
+    /// 向当前文档添加水平线。
     /// </summary>
-    /// <param name="index">要删除的内嵌形状索引</param>
-    void Delete(int index);
+    /// <param name="range">可选项。Microsoft Word 放置水平线之上的范围。如果省略此参数，Word 将在当前选定内容之上放置水平线。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? AddHorizontalLineStandard(IWordRange? range = null);
 
     /// <summary>
-    /// 删除所有内嵌形状
+    /// 基于图像文件向当前文档添加图片项目符号。
     /// </summary>
-    void DeleteAll();
+    /// <param name="fileName">必需。要用作图片项目符号的图像的文件名。</param>
+    /// <param name="range">可选项。Microsoft Word 添加图片项目符号的范围。Word 将图片项目符号添加到范围内的每个段落。如果省略此参数，Word 将图片项目符号添加到当前选定内容中的每个段落。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? AddPictureBullet(string fileName, IWordRange? range = null);
 
     /// <summary>
-    /// 查找指定类型的内嵌形状
+    /// 将指定类型的图表作为内嵌形状插入活动文档，并打开 Microsoft Office Excel，其中包含 Microsoft Office Word 用于创建图表的默认数据的工作表。
     /// </summary>
-    /// <param name="type">形状类型</param>
-    /// <returns>符合条件的内嵌形状集合</returns>
-    IEnumerable<IWordInlineShape> FindByType(WdInlineShapeType type);
+    /// <param name="type">指定要创建的图表类型。</param>
+    /// <param name="range">指定图表绑定到的文本。如果指定了 Range，图表将放置在范围中第一个段落的开头。如果省略此参数，则自动选择范围，图表相对于页面的顶部和左侧边缘定位。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? AddChart([ComNamespace("MsCore")] XlChartType type = XlChartType.xlArea, IWordRange? range = null);
+
+    /// <summary>
+    /// 添加具有指定样式和类型的图表。
+    /// </summary>
+    /// <param name="style">图表样式。</param>
+    /// <param name="type">图表类型。</param>
+    /// <param name="range">放置图表的范围。</param>
+    /// <param name="newLayout">新布局。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? AddChart2(int style = -1, [ComNamespace("MsCore")] XlChartType type = XlChartType.xlArea, IWordRange? range = null, object? newLayout = null);
+
+    /// <summary>
+    /// 将 SmartArt 图形作为内嵌形状插入活动文档。
+    /// </summary>
+    /// <param name="layout">指定 SmartArt 图形布局的 SmartArtLayout 对象。</param>
+    /// <param name="range">指定 SmartArt 图形绑定到的文本。如果指定了 Range，SmartArt 图形将放置在范围中第一个段落的开头。如果省略此参数，则自动选择范围，SmartArt 图形相对于页面的顶部和左侧边缘定位。</param>
+    /// <returns>要插入的 SmartArt 图形。</returns>
+    IWordInlineShape? AddSmartArt(IOfficeSmartArtLayout layout, IWordRange? range = null);
+
+    /// <summary>
+    /// 添加 Web 视频作为内嵌形状。
+    /// </summary>
+    /// <param name="embedCode">嵌入代码。</param>
+    /// <param name="videoWidth">视频宽度。</param>
+    /// <param name="videoHeight">视频高度。</param>
+    /// <param name="posterFrameImage">海报帧图像。</param>
+    /// <param name="url">视频 URL。</param>
+    /// <param name="range">放置视频的范围。</param>
+    /// <returns>新创建的 InlineShape 对象。</returns>
+    IWordInlineShape? AddWebVideo(string embedCode, int? videoWidth, int? videoHeight, string? posterFrameImage = null, string? url = null, IWordRange? range = null);
+
 }
