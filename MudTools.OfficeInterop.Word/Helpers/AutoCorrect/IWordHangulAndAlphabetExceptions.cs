b66c0640db4379977f6af23b43b442a053124baa
倔强的石头 <413901125@qq.com>
2026-01-06 17:07:57 +0800

    refactor(Word): 重构朝鲜文和字母自动更正异常接口
    - 添加 ComCollectionWrap 特性到 IWordHangulAndAlphabetExceptions 接口
    - 更新接口继承关系，实现 IOfficeObject 接口
    - 将返回类型修改为可空类型 IWordHangulAndAlphabetException?
    - 添加通过字符串名称索引器的重载方法
    - 移除内部实现类 WordHangulAndAlphabetExceptions
    - 更新集合元素类型为可空类型 IEnumerable<IWordHangulAndAlphabetException?>

--- MudTools.OfficeInterop.Word/Helpers/AutoCorrect/IWordHangulAndAlphabetExceptions.cs
+++ MudTools.OfficeInterop.Word/Helpers/AutoCorrect/IWordHangulAndAlphabetExceptions.cs
@@ -12,7 +12,8 @@ namespace MudTools.OfficeInterop.Word;
 /// <para>注：使用 HangulAndAlphabetExceptions 属性可返回 HangulAndAlphabetExceptions 集合。</para>
 /// <para>注：使用 HangulAndAlphabetExceptions(index)（其中 index 是朝鲜文或字母自动更正异常名称或索引号）返回单个 HangulAndAlphabetException 对象。</para>
 /// </summary>
-public interface IWordHangulAndAlphabetExceptions : IEnumerable<IWordHangulAndAlphabetException>, IDisposable
+[ComCollectionWrap(ComNamespace = "MsWord")]
+public interface IWordHangulAndAlphabetExceptions : IEnumerable<IWordHangulAndAlphabetException?>, IOfficeObject<IWordHangulAndAlphabetExceptions, MsWord.HangulAndAlphabetExceptions>, IDisposable
 {
     /// <summary>
     /// 获取与该对象关联的 Word 应用程序。
@@ -35,12 +36,19 @@ public interface IWordHangulAndAlphabetExceptions : IEnumerable<IWordHangulAndAl
     /// </summary>
     /// <param name="index">索引号（整数）或异常名称（字符串）。</param>
     /// <returns>指定的朝鲜文和字母自动更正异常对象。</returns>
-    IWordHangulAndAlphabetException this[object index] { get; }
+    IWordHangulAndAlphabetException? this[object index] { get; }
+
+    /// <summary>
+    /// 通过索引号或异常名称获取单个朝鲜文和字母自动更正异常。
+    /// </summary>
+    /// <param name="name">索引号（整数）或异常名称（字符串）。</param>
+    /// <returns>指定的朝鲜文和字母自动更正异常对象。</returns>
+    IWordHangulAndAlphabetException? this[string name] { get; }
 
     /// <summary>
     /// 将项添加到朝鲜文和字母自动更正异常列表中。
     /// </summary>
     /// <param name="name">要添加的异常名称。</param>
     /// <returns>表示添加的异常的对象。</returns>
-    IWordHangulAndAlphabetException Add(string name);
+    IWordHangulAndAlphabetException? Add(string name);
 }
