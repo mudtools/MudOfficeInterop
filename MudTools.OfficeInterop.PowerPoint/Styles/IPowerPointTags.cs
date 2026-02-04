922f2345871961277da03fac8348b3c773c88efe
倔强的石头 <413901125@qq.com>
2026-02-04 17:43:34 +0800

    refactor(PowerPointTags): 重构标签接口并移除实现类
    - 简化 IPowerPointTags 接口定义，移除冗余方法
    - 添加 ComCollectionWrap 和 ComPropertyWrap 属性
    - 删除 PowerPointTags 实现类，后续将由其他方式实现

--- MudTools.OfficeInterop.PowerPoint/Styles/IPowerPointTags.cs
+++ MudTools.OfficeInterop.PowerPoint/Styles/IPowerPointTags.cs
@@ -9,118 +9,75 @@ namespace MudTools.OfficeInterop.PowerPoint;
 
 
 /// <summary>
-/// PowerPoint 标签集合接口
+/// 表示 PowerPoint 对象中的标签集合。
 /// </summary>
-public interface IPowerPointTags : IDisposable
+[ComCollectionWrap(ComNamespace = "MsPowerPoint", NoneDisposed = false)]
+public interface IPowerPointTags : IEnumerable<string?>, IDisposable
 {
     /// <summary>
-    /// 获取标签数量
+    /// 获取集合中的标签数量。
     /// </summary>
+    /// <value>集合中的标签数量。</value>
     int Count { get; }
 
     /// <summary>
-    /// 获取父对象
+    /// 获取创建此标签集合的 PowerPoint 应用程序实例。
     /// </summary>
+    /// <value>表示 PowerPoint 应用程序的 <see cref="IPowerPointApplication"/> 对象。</value>
+    [ComPropertyWrap(NeedDispose = false)]
+    IPowerPointApplication? Application { get; }
+
+    /// <summary>
+    /// 获取此标签集合的父对象。
+    /// </summary>
+    /// <value>表示此标签集合父对象的 <see cref="object"/>。</value>
     object? Parent { get; }
 
     /// <summary>
-    /// 根据索引获取标签值
+    /// 通过名称获取集合中的指定标签值。
     /// </summary>
-    string this[int index] { get; }
+    /// <param name="name">要获取的标签的名称。</param>
+    /// <value>指定名称对应的标签值字符串。</value>
+    string? this[string name] { get; }
 
     /// <summary>
-    /// 根据名称获取标签值
+    /// 在标签集合中添加新标签。
     /// </summary>
-    string this[string name] { get; }
-
-
-    /// <summary>
-    /// 获取标签名称
-    /// </summary>
-    /// <param name="index">标签索引</param>
-    /// <returns>标签名称</returns>
-    string Name(int index);
-
-    /// <summary>
-    /// 获取标签值
-    /// </summary>
-    /// <param name="index">标签索引</param>
-    /// <returns>标签值</returns>
-    string Value(int index);
-
-    /// <summary>
-    /// 添加标签
-    /// </summary>
-    /// <param name="name">标签名称</param>
-    /// <param name="value">标签值</param>
+    /// <param name="name">新标签的名称。</param>
+    /// <param name="value">新标签的值。</param>
     void Add(string name, string value);
 
     /// <summary>
-    /// 删除标签
+    /// 删除指定名称的标签。
     /// </summary>
-    /// <param name="name">标签名称</param>
+    /// <param name="name">要删除的标签的名称。</param>
     void Delete(string name);
 
     /// <summary>
-    /// 清除所有标签
+    /// 添加二进制数据的标签。
     /// </summary>
-    void Clear();
+    /// <param name="name">标签的名称。</param>
+    /// <param name="filePath">包含二进制数据的文件路径。</param>
+    void AddBinary(string name, string filePath);
 
     /// <summary>
-    /// 检查标签是否存在
+    /// 获取二进制标签的值。
     /// </summary>
-    /// <param name="name">标签名称</param>
-    /// <returns>是否存在</returns>
-    bool Contains(string name);
+    /// <param name="name">二进制标签的名称。</param>
+    /// <returns>二进制标签的值。</returns>
+    int? BinaryValue(string name);
 
     /// <summary>
-    /// 更新标签值
+    /// 通过索引获取标签的名称。
     /// </summary>
-    /// <param name="name">标签名称</param>
-    /// <param name="value">新标签值</param>
-    void Update(string name, string value);
+    /// <param name="index">标签的索引（从1开始）。</param>
+    /// <returns>指定索引处标签的名称。</returns>
+    string? Name(int index);
 
     /// <summary>
-    /// 获取所有标签名称
+    /// 通过索引获取标签的值。
     /// </summary>
-    /// <returns>标签名称列表</returns>
-    IEnumerable<string> GetAllNames();
-
-    /// <summary>
-    /// 获取所有标签键值对
-    /// </summary>
-    /// <returns>标签键值对字典</returns>
-    IDictionary<string, string> GetAllTags();
-
-    /// <summary>
-    /// 导出标签到文件
-    /// </summary>
-    /// <param name="fileName">文件路径</param>
-    void ExportToFile(string fileName);
-
-    /// <summary>
-    /// 从文件导入标签
-    /// </summary>
-    /// <param name="fileName">文件路径</param>
-    void ImportFromFile(string fileName);
-
-    /// <summary>
-    /// 查找符合名称条件的标签
-    /// </summary>
-    /// <param name="namePredicate">名称条件</param>
-    /// <returns>符合条件的标签列表</returns>
-    IEnumerable<KeyValuePair<string, string>> FindByName(Func<string, bool> namePredicate);
-
-    /// <summary>
-    /// 查找符合值条件的标签
-    /// </summary>
-    /// <param name="valuePredicate">值条件</param>
-    /// <returns>符合条件的标签列表</returns>
-    IEnumerable<KeyValuePair<string, string>> FindByValue(Func<string, bool> valuePredicate);
-
-    /// <summary>
-    /// 获取标签集合信息
-    /// </summary>
-    /// <returns>标签集合信息字符串</returns>
-    string GetTagsInfo();
+    /// <param name="index">标签的索引（从1开始）。</param>
+    /// <returns>指定索引处标签的值。</returns>
+    string? Value(int index);
 }
